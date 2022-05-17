81fad1f119de78167cf4d62403a6081a167424ca
tpb1908 <tpb1908dev@gmail.com>
2022-05-17 12:56:44 +0100

    Rebuild

---
+++ bundle.js
@@ -0,0 +1,34 @@
+import * as validatorLibrary from 'validator';
+import { isValidPostcode, parsePostcode } from './lib/validation';
+import { formatDateDMYHMS, formatAddress, formatShortAddress, formatPostcode } from './lib/formatting';
+import { checkIfTerminatedPostcode, checkIfValidPostcode, checkPostcodeForCoordinates } from './lib/check';
+/**
+ * Functions for common string formatting
+ */
+
+export const formatter = {
+  formatAddress,
+  formatShortAddress,
+  formatDateDMYHMS,
+  formatPostcode
+};
+/**
+ * Functions which perform common validations
+ * These functions are all synchronous and run locally
+ */
+
+export const validator = {
+  isValidPostcode,
+  parsePostcode,
+  ...validatorLibrary
+};
+/**
+ * Functions which perform external checks
+ * These functions may be asynchronous and rely on external services
+ */
+
+export const checker = {
+  checkIfValidPostcode,
+  checkIfTerminatedPostcode,
+  checkPostcodeForCoordinates
+};
--- main.js
+++ main.js
@@ -1,34 +1,41 @@
-import * as validatorLibrary from 'validator';
-import { isValidPostcode, parsePostcode } from './lib/validation';
-import { formatDateDMYHMS, formatAddress, formatShortAddress, formatPostcode } from './lib/formatting';
-import { checkIfTerminatedPostcode, checkIfValidPostcode, checkPostcodeForCoordinates } from './lib/check';
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+exports.__esModule = true;
+exports.checker = exports.validator = exports.formatter = void 0;
+var validatorLibrary = require("validator");
+var validation_1 = require("./lib/validation");
+var formatting_1 = require("./lib/formatting");
+var check_1 = require("./lib/check");
 /**
  * Functions for common string formatting
  */
-
-export const formatter = {
-  formatAddress,
-  formatShortAddress,
-  formatDateDMYHMS,
-  formatPostcode
+exports.formatter = {
+    formatAddress: formatting_1.formatAddress,
+    formatShortAddress: formatting_1.formatShortAddress,
+    formatDateDMYHMS: formatting_1.formatDateDMYHMS,
+    formatPostcode: formatting_1.formatPostcode
 };
 /**
  * Functions which perform common validations
  * These functions are all synchronous and run locally
  */
-
-export const validator = {
-  isValidPostcode,
-  parsePostcode,
-  ...validatorLibrary
-};
+exports.validator = __assign({ isValidPostcode: validation_1.isValidPostcode, parsePostcode: validation_1.parsePostcode }, validatorLibrary);
 /**
  * Functions which perform external checks
  * These functions may be asynchronous and rely on external services
  */
-
-export const checker = {
-  checkIfValidPostcode,
-  checkIfTerminatedPostcode,
-  checkPostcodeForCoordinates
+exports.checker = {
+    checkIfValidPostcode: check_1.checkIfValidPostcode,
+    checkIfTerminatedPostcode: check_1.checkIfTerminatedPostcode,
+    checkPostcodeForCoordinates: check_1.checkPostcodeForCoordinates
 };
